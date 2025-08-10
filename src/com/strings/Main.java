fc99dd55f4225ed9531d2ca98742eb60de59b050
Julia Marushchenko <marushchenko.yulia@gmail.com>
2025-08-10 14:40:45 +0200

    Add files via upload
    Committing Main class

---
+++ src/com/strings/Main.java
@@ -0,0 +1,17 @@
+// Java program to find index of a word
+
+package com.strings;
+
+// Main class
+public class Main {
+
+    // Main method to run java program
+    public static void main(String[] args) {
+
+        // Creating string to practice
+        String str = "Hello planet Earth. You are great planet.";
+
+        // Finding index of word 'great' and printing to console
+        System.out.println(str.indexOf("great")); // Output: 28
+    }
+}
